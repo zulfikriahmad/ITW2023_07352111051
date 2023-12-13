3c11d6971681b01dcb4080e0fa281c2ae46362ec
zulfikriahmad <zulfikriahmad321@gnail.com>
2023-12-13 08:56:21 +0700

    mengerjakan latihan 5,6dan7

---
+++ latihan/latihan6/script.js
@@ -0,0 +1,8 @@
+const menuToogle = document.querySelector('.menu-toggle input');
+const nav = document.querySelector('nav ul');
+
+
+
+menuToogle.addEventListener('click', function() {
+    nav.classList.toggle('slide');
+});
